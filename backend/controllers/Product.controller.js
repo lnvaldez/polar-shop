0fb7dd991b307e853874b40c426e45b7b4755503
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-21 10:41:41 -0300

    feat(product): function to get product by id

--- backend/controllers/Product.controller.js
+++ backend/controllers/Product.controller.js
@@ -30,9 +30,25 @@ const addProduct = async (req, res) => {
 //* ..Read
 const getAllProducts = async (req, res) => {
   try {
-    const products = await Products.find().sort({ createdAt: -1 });
+    const products = await Product.find().sort({ createdAt: -1 });
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+};
+
+const getProduct = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  res.status(200).json(product);
 };
