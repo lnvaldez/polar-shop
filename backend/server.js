62a76c3a19e33bf936111dc101cbe52e8ee37c2c
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-20 22:55:21 -0300

    refactor: express connection awaits for mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db.config");
 
 const PORT = process.env.EXPRESS_PORT || 8080;
 
 const app = express();
 
-app.listen(8080, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`🚀 Express server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start server:", error);
+  }
+};
+
+startServer();
