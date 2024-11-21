6f5b6b7384bdcc568ce2ee3d54e1f5646fcc1f06
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-21 10:32:16 -0300

    feat(product): function to get all products

--- backend/controllers/Product.controller.js
+++ backend/controllers/Product.controller.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const Product = require("../models");
 
-// CRUD
+/* CRUD */
 
-// ..Create
+//* ..Create
 const addProduct = async (req, res) => {
   const { name, price, description } = req.body;
 
@@ -24,5 +24,15 @@ const addProduct = async (req, res) => {
     res.status(200).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+};
+
+//* ..Read
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Products.find().sort({ createdAt: -1 });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
