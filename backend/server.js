64bb91e07b8fe572422228986ab6ed7c2ee1e5ea
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-21 11:04:03 -0300

    feat(app): log incoming reqs path and method

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 const startServer = async () => {
   try {
     await connectDB();
