a558d319c65079f57616c09f4387410713dd9a65
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-22 11:05:29 -0300

    chore: include admin routes on global routes file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,14 @@ const express = require("express");
 //* Route files
 const productRoutes = require("./Product.routes");
 const userRoutes = require("./User.routes");
+const adminRoutes = require("./Admin.routes");
 
 const router = express.Router();
 
 //* Grouped routes
 router.use("/products", productRoutes);
 router.use("/users", userRoutes);
+router.use("/admin", adminRoutes);
 // TODO router.use("/orders", orderRoutes);
 
 module.exports = router;
