2e3d45ecc08a2c96d7ee7fb8073d706c2e2ed74d
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-21 22:21:40 -0300

    feat(user): register static method for model

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -1,4 +1,7 @@
+/* Imports */
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -19,3 +22,25 @@ const userSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("User", userSchema);
+
+//* Static
+userSchema.statics.register = async function (name, password) {
+  if (!name || !password) {
+    throw Error("Fill out all required fields");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
+  const exists = await this.findOne({ name });
+
+  if (exists) {
+    throw Error("Name already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ name, password: hash });
+};
