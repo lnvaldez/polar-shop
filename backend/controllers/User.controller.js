7fc80b1ffe935e6dc175386ec9ffcaa200775a02
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-21 22:42:32 -0300

    feat(user): login function

--- backend/controllers/User.controller.js
+++ backend/controllers/User.controller.js
@@ -4,3 +4,17 @@ const jwt = require("jsonwebtoken");
 const generateToken = (_id) => {
   return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
 };
+
+const login = async (req, res) => {
+  const { name, password } = req.body;
+
+  try {
+    const user = await User.login(name, password);
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({ name, token });
+  } catch (error) {
+    res.statuso(400).json({ error: error.message });
+  }
+};
