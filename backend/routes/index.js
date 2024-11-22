6bef5f81e90e1be36e0bd4b4e8c54962ab448c9c
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-21 22:46:59 -0300

    feat(user): include user routes on global routes file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,13 @@
 const express = require("express");
 //* Route files
 const productRoutes = require("./Product.routes");
+const userRoutes = require("./User.routes");
 
 const router = express.Router();
 
 //* Grouped routes
 router.use("/products", productRoutes);
-// TODO router.use("/users", userRoutes);
+router.use("/users", userRoutes);
 // TODO router.use("/orders", orderRoutes);
 
 module.exports = router;
