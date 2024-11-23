d6a07fc9c7e0e04268a7569baedd7c1600f818f7
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-23 20:27:01 -0300

    feat(products): get all available products

--- frontend/internal/repository/mongodb_repo.go
+++ frontend/internal/repository/mongodb_repo.go
@@ -18,3 +18,18 @@ func NewMongoRepo(client *mongo.Client, dbName string) *MongoRepo {
 		db: client.Database(dbName),
 	}
 }
+
+func (r *MongoRepo) GetAllAvailableProducts() ([]models.Product, error) {
+	collection := r.db.Collection("products")
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.TODO())
+
+	var products []models.Product
+	if err = cursor.All(context.TODO(), &products); err != nil {
+		return nil, err
+	}
+	return products, nil
+}
