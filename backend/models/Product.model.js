e18900a1767a551d1a29e8712e0dee76004ac1dc
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-23 11:35:09 -0300

    refactor(product): rename field to 'isAvailable'

--- backend/models/Product.model.js
+++ backend/models/Product.model.js
@@ -24,7 +24,7 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
-    available: {
+    isAvailable: {
       type: Boolean,
       default: false,
     },
