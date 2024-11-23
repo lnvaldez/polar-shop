187ff43e77b41471768e8125d9c25497f013e442
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-23 14:01:21 -0300

    chore: go imports

--- frontend/main.go
+++ frontend/main.go
@@ -1,5 +1,18 @@
 package main  
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 func main() {
 	
 }
