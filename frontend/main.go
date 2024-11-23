1ef2aa03ef0336f45cc233a3da08f96909943ee4
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-23 14:13:22 -0300

    test: db data find

--- frontend/main.go
+++ frontend/main.go
@@ -32,4 +32,22 @@ func main() {
 		}
 	}()
 	
+	coll := client.Database("polar_shop").Collection("products")
+	name := "Bonnet"
+
+	var result bson.M 
+	err = coll.FindOne(context.TODO(), bson.D{{"name", name}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("Product not found with name $s\n", name)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	jsonData, err := json.MarshalIndent(result, "", "   ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
 }
