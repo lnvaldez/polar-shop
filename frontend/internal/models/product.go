10f421bf1039450de2963e80a9053967910421a9
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-24 11:51:37 -0300

    fix: price should be int32

--- frontend/internal/models/product.go
+++ frontend/internal/models/product.go
@@ -4,7 +4,7 @@ type Product struct {
 	ID string `bson:"_id,omitempty"`
 	Name string `bson:"name"`
 	Category string `bson:"category"`
-	Price string `bson:"price"`
+	Price int32 `bson:"price"`
 	Quantity string `bson:"quantity"`
 	Image string `bson:"image"`
 	Status bool `bson:"isAvailable"`
