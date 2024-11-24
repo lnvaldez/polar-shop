f1f35cceb4291bc1ac4aa9777eb7aab1caaa46c1
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-24 14:23:09 -0300

    feat: initialize mongodb function

--- frontend/main.go
+++ frontend/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"os"
 	"context"
+	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var mongoClient *mongo.Client
@@ -18,7 +20,19 @@ func init() {
 		panic(err)
 	}
 
-	mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	mongoClient, err = mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = mongoClient.Ping(context.Background(), readpref.Primary())
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("✅ Connected to MongoDB")
 }
 
 func main() {
