98f0ae182cf9d5f9491e8f29dedb3ea3f27550df
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-24 14:13:28 -0300

    feat: func gets all prods

--- frontend/db/product_db.go
+++ frontend/db/product_db.go
@@ -1,11 +1,32 @@
-package db 
+package db
 
 import (
+	"fmt"
+	"context"
+
 	"go.mongodb.org/mongo-driver/mongo"
-	
+	"go.mongodb.org/mongo-driver/bson"
+
 	"polar-shop/models"
 )
 
 type ProductDB struct {
 	MongoCollection *mongo.Collection
+}
+
+func (db *ProductDB) GetAvailableProducts(prod * model.Product) ([]model.Product, error) {
+	result, err := db.MongoCollection.Find(context.Background(), bson.D{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var prods []model.Product
+	err = result.All(context.Background(), &prods)
+	
+	if err != nil {
+		return nil, fmt.Errorf("error %s", err.Error())
+	}
+
+	return prods, nil
 }
