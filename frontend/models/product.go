d8c7f5e478d14afe61791ab5f3ebb74608ede079
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-24 14:03:05 -0300

    feat: define product struct

---
+++ frontend/models/product.go
@@ -0,0 +1,12 @@
+package model 
+
+type Product struct {
+	ID string `bson:"_id,omitempty"`
+	Name string `bson:"name"`
+	Category string `bson:"category"`
+	Price int32 `bson:"price"`
+	Quantity string `bson:"quantity"`
+	Image string `bson:"image"`
+	Status bool `bson:"isAvailable"`
+	Description string `bson:"description"`
+}
