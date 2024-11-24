064ee1600f2c01d852b4fc1fbc3b6c4d9b1d20d0
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-24 14:48:27 -0300

    feat: get all prods func http

--- frontend/service/product_service.go
+++ frontend/service/product_service.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"net/http"
+	"encoding/json"
+	"log"
+	"polar-shop/db"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -15,4 +18,23 @@ type Response struct {
 	Error string      `json:"error,omitempty"`
 }
 
-func (svc *ProductService) GetAvailableProducts(w http.ResponseWriter, r *http.Request) {}
+func (svc *ProductService) GetAvailableProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
+	res := &Response{}
+	defer json.NewEncoder(w).Encode(res)
+
+	db := db.ProductDB{MongoCollection: svc.MongoCollection}
+
+	prods, err := db.GetAvailableProducts()
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("error; ", err)
+		res.Error = err.Error()
+		return
+	}
+
+	res.Data = prods 
+	w.WriteHeader(http.StatusOK)
+}
