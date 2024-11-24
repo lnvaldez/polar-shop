4a0db08c0498448709b58a7ebf188ae0e046ae82
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-24 14:36:16 -0300

    feat: define service func for prods

--- frontend/service/product_service.go
+++ frontend/service/product_service.go
@@ -1,11 +1,18 @@
 package service
 
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type ProductService struct {
 	MongoCollection *mongo.Collection
 }
 
 type Response struct {
-	Data interface{} `json:"data,omitempty"`
-	Error string `json:"error,omitempty"`
+	Data  interface{} `json:"data,omitempty"`
+	Error string      `json:"error,omitempty"`
 }
 
+func (svc *ProductService) GetAvailableProducts(w http.ResponseWriter, r *http.Request) {}
