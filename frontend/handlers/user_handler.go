9dad41dc1745b5d245478094e74ee7681bb9ec1e
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-24 15:33:47 -0300

    feat: write render reg page handler func

--- frontend/handlers/user_handler.go
+++ frontend/handlers/user_handler.go
@@ -5,6 +5,16 @@ import (
 	"html/template"
 )
 
+func RenderRegisterPage(tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := tmpl.ExecuteTemplate(w, "register.html", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return 
+		}
+	}
+}
+
 func RenderLoginPage(tmpl *template.Template) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         err := tmpl.ExecuteTemplate(w, "login.html", nil)
