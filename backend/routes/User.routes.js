586a993047b2db8938bc15d3f5f0805463264187
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-25 16:58:39 -0300

    feat: get method for logout route

--- backend/routes/User.routes.js
+++ backend/routes/User.routes.js
@@ -1,12 +1,15 @@
 /* Imports */
 const express = require("express");
 
-const { register, login } = require("../controllers/User.controller");
+const { register, login, logout } = require("../controllers/User.controller");
 
 const router = express.Router();
 
 // POST
 router.post("/register", register);
 router.post("/login", login);
+
+// GET
+router.get("/logout", logout);
 
 module.exports = router;
