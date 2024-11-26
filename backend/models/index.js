48a51247c5815264e9a78ffa54e257b8bcfea139
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-26 13:54:48 -0300

    chore: include order model on compound export

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,5 @@
 const User = require("./User.model");
 const Product = require("./Product.model");
+const Order = require("./Order.model");
 
-module.exports = { User, Product };
+module.exports = { User, Product, Order };
