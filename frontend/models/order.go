437b28d5b9cdcb0c5fba5f4d87aa6b9c9304c050
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-26 11:55:11 -0300

    fix: incorrect field name for model

--- frontend/models/order.go
+++ frontend/models/order.go
@@ -2,7 +2,7 @@ package model
 
 type Order struct {
 	ID string `bson:"_id,omitempty"`
-	CustomerName string `bson:"name"`
+	CustomerName string `bson:"customerName"`
 	Email string `bson:"email"`
 	Address string `bson:"address"`
 	Status string `bson:"status"`
