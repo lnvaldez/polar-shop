7c1a8a7a7fd0c512b1b67197d12f7b8d02c49471
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-26 21:34:55 -0300

    feat: add product and quantity fields

--- frontend/models/order.go
+++ frontend/models/order.go
@@ -4,6 +4,8 @@ type Order struct {
 	ID string `bson:"_id,omitempty"`
 	CustomerName string `bson:"customerName"`
 	Email string `bson:"email"`
+	Product string `bson:"productName"`
+	ProductQuantity int32 `bson:"productQuantity"`
 	Address string `bson:"address"`
 	Status string `bson:"status"`
 }
