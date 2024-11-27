bd9557227a8964256184d340d7ae80dcd6c72aa3
Lucas Valdez <lnvaldez24@gmail.com>
2024-11-26 21:38:36 -0300

    feat: render order product page

--- frontend/handlers/product_handler.go
+++ frontend/handlers/product_handler.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
-	"polar-shop/service"
 	"polar-shop/models"
+	"polar-shop/service"
 
-	"net/http"
 	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func RenderAvailableProductsPage(productService *service.ProductService, tmpl *template.Template) http.HandlerFunc {
@@ -20,6 +22,27 @@ func RenderAvailableProductsPage(productService *service.ProductService, tmpl *t
             Products []model.Product
         }{
             Products: prods,
+        }
+
+        err = tmpl.ExecuteTemplate(w, "layout", data)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    }
+}
+
+func RenderOrderProductPage(productService *service.ProductService, tmpl *template.Template) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        prodID := mux.Vars(r)["id"]
+
+        prod, err := productService.GetProductById(prodID)
+        if err != nil {
+            http.Error(w, "Product not found", http.StatusNotFound)
+            return
+        }
+
+        data := map[string]interface{}{
+            "Product": prod,
         }
 
         err = tmpl.ExecuteTemplate(w, "layout", data)
